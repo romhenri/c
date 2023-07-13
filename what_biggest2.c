cb1dbcbf2aa2280beb63a35809136032a8e4f6c3
Rômulo Henri <123867521+romhenri@users.noreply.github.com>
2023-07-13 20:41:24 -0300

    add: what_biggest2.c (NotWorking)

---
+++ what_biggest2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int what_biggest(arr)
+{
+	int biggest = -1;
+
+	for (int i = 0; i < 5; i++)
+	{
+		if (arr[i] > biggest)
+		{
+			biggest = arr[i];
+		}
+	}
+
+	return biggest
+}
+
+int main(void)
+{
+	int arr[5] = {0};
+
+	for (int i = 0; i < 5; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		printf("%d, ", arr[i]);
+	}
+
+	int biggest = what_biggest(arr);
+	printf("biggest: %d", biggest);
+
+	return 0;
+}
