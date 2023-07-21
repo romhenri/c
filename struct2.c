1809c3ecc71231ef2e984e0974186b3ed169b92b
Rômulo Henri <123867521+romhenri@users.noreply.github.com>
2023-07-21 20:36:44 -0300

    add: struct2.c

---
+++ struct2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#define TAMANHO_NOME 100
+#define QTDE_PESSOAS 100
+
+struct Human
+{
+    int id;
+    int age;
+    char name[TAMANHO_NOME];
+    int size;
+};
+
+int main()
+{
+    struct Human personagens[QTDE_PESSOAS];
+
+    for (int i = 0; i < QTDE_PESSOAS; i++)
+    {
+        scanf("%d\n", &personagens[i].id);
+        scanf("%[^\n]s", &personagens[i].name);
+        scanf("%f", &personagens[i].size);
+    }
+
+    for (int i = 0; i < QTDE_PESSOAS; i++)
+    {
+        printf("Idade: %d\n", personagens[i].id);
+        printf("Idade: %s\n", personagens[i].name);
+        printf("Idade: %f\n", personagens[i].size);
+    }
+
+    return 0;
+}
