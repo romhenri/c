a0564eb7d56faa6b46791cb6d4adcdd7436d4ca7
Rômulo Henri <123867521+romhenri@users.noreply.github.com>
2023-07-22 21:04:29 -0300

    add: math.c

---
+++ math.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	// Arredonda para cima:
+	printf("%f \n", ceil(1.4));
+	// Arredonda para baixo:
+	printf("%f \n", floor(1.4));
+
+	// Raiz Quadrada:
+	printf("%f \n", sqrt(16));
+
+	// Raiz Cúbica:
+	printf("%f \n", cbrt(1728));
+
+	// X elevado a Y:
+	printf("%f \n", pow(4, 2));
+}
