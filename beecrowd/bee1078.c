0239411ff8116668a021d2080dc2a72a728b8b71
Rômulo Henri <romulohenri000@gmail.com>
2024-01-25 09:56:18 -0300

    add: bee1078.c

---
+++ beecrowd/bee1078.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main() {
+  int number;
+  
+  scanf("%d", &number);
+  
+  printf("1 x %d = %d\n", number, (number * 1));
+  printf("2 x %d = %d\n", number, (number * 2));
+  printf("3 x %d = %d\n", number, (number * 3));
+  printf("4 x %d = %d\n", number, (number * 4));
+  printf("5 x %d = %d\n", number, (number * 5));
+  printf("6 x %d = %d\n", number, (number * 6));
+  printf("7 x %d = %d\n", number, (number * 7));
+  printf("8 x %d = %d\n", number, (number * 8));
+  printf("9 x %d = %d\n", number, (number * 9));
+  printf("10 x %d = %d\n", number, (number * 10));
+  
+  return 0;
+}
